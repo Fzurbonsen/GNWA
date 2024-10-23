44fe58553b02db0379c546c577debab45184dc08
Frederic zurBonsen <fzurbonsen@safari-proxy>
2024-10-23 22:48:28 +0200

    fixed nt table

--- src/gnwa.c
+++ src/gnwa.c
@@ -49,7 +49,7 @@ int8_t* gnwa_create_score_matrix(int32_t match, int32_t mismatch) {
 }
 
 
-// Function to create an nt table
+// Function to create nt table
 int8_t* gssw_create_nt_table(void) {
     int8_t* ret_nt_table = calloc(128, sizeof(int8_t));
     int8_t nt_table[128] = {
